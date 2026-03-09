4b1f4f6abc15e28ecfe0c2739c7d8cbec4af1b02
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-09 17:16:07 -0300

    main function complete

--- Legends of the Terminal.py
+++ Legends of the Terminal.py
@@ -176,6 +176,36 @@ def quit_game():
     pass
 # ---------------------------   L E G E N D S   O F   T H E   T E R M I N A L   ---------------------------------
 def main():
-    pass
+        welcome()
+        hero_name()
+        while True:
+            menu()
+            choice = input(f'Whats your next move, {player["name"]}? >')
+            if choice == '1':
+                explore()
+            elif choice == '2':
+                fight_boss()
+            elif choice == '3':
+                show_inventory()
+            elif choice == '4':
+                upgrade_characters()
+            elif choice == '5':
+                shop()
+            elif choice == '6':
+                sell()
+            elif choice == '7':
+                rest()
+            elif choice == '8':
+                show_stats()
+            elif choice == '9':
+                use_item()
+            elif choice == '10':
+                drop_item()
+            elif choice == 'q':
+                quit_game()
+                break
+            else:
+                print('That\'s not in the menu adventurer.')
+
 if __name__ == '__main__':
     main()
